a0719b3f1f3f67c20bc629e6de37ac9891474810
Alex Gaynor <alex@alloy.us>
2019-09-25 09:32:42 -0400

    Upgrade ChromeOS version for M77 (#9)

--- gsuite-device-update-notifier/main.js
+++ gsuite-device-update-notifier/main.js
@@ -4,8 +4,8 @@ var ANDROID_LAST_UPDATE = new Date(2019, 8, 3);
 var MINIMUM_IOS_VERSION = splitOSVersion("13.1");
 var IOS_LAST_UPDATE = new Date(2019, 8, 24);
 
-var MINIMUM_CHROMEOS_VERSION = splitOSVersion("76.0.3809.136");
-var CHROMEOS_LAST_UPDATE = new Date(2019, 8, 10);
+var MINIMUM_CHROMEOS_VERSION = splitOSVersion("77.0.3865.93");
+var CHROMEOS_LAST_UPDATE = new Date(2019, 8, 25);
 
 
 function checkOutdatedMobileDevice(device) {
