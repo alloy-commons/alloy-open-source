d0d47277135a2bfba39d88929e949d8902408397
Alex Gaynor <alex@alloy.us>
2019-11-04 13:49:42 -0500

    Update for newest Android release (#20)

--- gsuite-device-update-notifier/main.js
+++ gsuite-device-update-notifier/main.js
@@ -1,5 +1,5 @@
-var MINIMUM_ANDROID_VERSION = new Date(2019, 9, 5);
-var ANDROID_LAST_UPDATE = new Date(2019, 9, 9);
+var MINIMUM_ANDROID_VERSION = new Date(2019, 10, 5);
+var ANDROID_LAST_UPDATE = new Date(2019, 10, 4);
 
 var MINIMUM_IOS_VERSION = splitOSVersion("13.2");
 var IOS_LAST_UPDATE = new Date(2019, 9, 29);
