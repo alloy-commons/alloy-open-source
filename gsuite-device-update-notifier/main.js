c4de5856524eb8cc303effc359c2fced709b1a8d
Alex Gaynor <alex@alloy.us>
2019-11-20 09:50:19 -0500

    [gsuite-update-notifier] bump for M78 bugfix release

--- gsuite-device-update-notifier/main.js
+++ gsuite-device-update-notifier/main.js
@@ -4,7 +4,7 @@ var ANDROID_LAST_UPDATE = new Date(2019, 10, 4);
 var MINIMUM_IOS_VERSION = splitOSVersion("13.2");
 var IOS_LAST_UPDATE = new Date(2019, 9, 29);
 
-var MINIMUM_CHROMEOS_VERSION = splitOSVersion("78.0.3904.92");
+var MINIMUM_CHROMEOS_VERSION = splitOSVersion("78.0.3904.106");
 var CHROMEOS_LAST_UPDATE = new Date(2019, 10, 6);
 
 var ANDROID_OUTDATED_MESSAGE = "\
