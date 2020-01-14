709c802c6a0ef9a59570570d807d931c24479177
Alex Gaynor <alex@alloy.us>
2020-01-14 14:02:15 -0600

    Update for ChromeOS 79.0.3945.123

--- gsuite-device-update-notifier/main.js
+++ gsuite-device-update-notifier/main.js
@@ -4,8 +4,8 @@ var ANDROID_LAST_UPDATE = new Date(2020, 0, 7);
 var MINIMUM_IOS_VERSION = splitOSVersion("13.3");
 var IOS_LAST_UPDATE = new Date(2019, 11, 10);
 
-var MINIMUM_CHROMEOS_VERSION = splitOSVersion("79.0.3945.86");
-var CHROMEOS_LAST_UPDATE = new Date(2019, 11, 19);
+var MINIMUM_CHROMEOS_VERSION = splitOSVersion("79.0.3945.123");
+var CHROMEOS_LAST_UPDATE = new Date(2019, 0, 14);
 
 var ANDROID_OUTDATED_MESSAGE = "\
 Hello,\n\
